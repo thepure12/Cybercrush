a76259e656a1057a99d265e41846374b8f34ce33
thpeure12 <tr3897@cs.ship.edu>
2014-03-01 03:58:36 -0500

    Initial code.

---
+++ Cybercrush-android/src/com/cybercrush/MainActivity.java
@@ -0,0 +1,20 @@
+package com.cybercrush;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+import cybercrush.Cybercrush;
+
+public class MainActivity extends AndroidApplication {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
+        cfg.useGL20 = false;
+        
+        initialize(new Cybercrush(), cfg);
+    }
+}
